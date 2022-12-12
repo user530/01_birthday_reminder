56d54c6cb1037867dd185924e58cb4a58bc1f53f
TMV <m.timarsuev@gmail.com>
2022-12-12 17:43:43 +0300

    Dynamic List + Clear btn

--- src/List.js
+++ src/List.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const List = () => {
+const List = ({ people }) => {
   return (
     <React.Fragment>
-      <h2>LIST</h2>
+      {people.map((personObj) => {
+        const { id, name, age, image } = personObj;
+
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
     </React.Fragment>
   );
 };
